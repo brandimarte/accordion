a3d2dc8ffdbeed5f1b345e3eae2fd1684537c6be
Pedro Brandimarte <pbrandimarte@vicomtech.org>
2025-12-24 20:12:45 +0100

    feat: chord seventh flat sixth (7b6)
    feat: chord nineth sus4 (9sus4)
    
    feat: chord maj9
    
    fix: play chord for new chords

--- js/utils.js
+++ js/utils.js
@@ -19,3 +19,23 @@ export function findClosestNoteToLeft(rootNote, rootNoteIndex, targetNoteChromat
   }
   return bestFitNote;
 }
+
+export function findClosestNoteToRight(rootNote, rootNoteIndex, targetNoteChromaticIndex) {
+  const enharmonicTargetNotes = indexToAllNotes[targetNoteChromaticIndex] || [];
+  const rootNoteBassIndex = bassNotes.indexOf(rootNote);
+
+  let bestFitNote = null;
+  let minDistance = Infinity;
+
+  for (const note of enharmonicTargetNotes) {
+    const noteBassIndex = bassNotes.indexOf(note);
+    if (noteBassIndex !== -1 && noteBassIndex > rootNoteBassIndex) {
+      const distance = noteBassIndex - rootNoteBassIndex;
+      if (distance < minDistance) {
+        minDistance = distance;
+        bestFitNote = note;
+      }
+    }
+  }
+  return bestFitNote;
+}
